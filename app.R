779d9df091c8a8ea60e4f90e3002df96845c0108
Maija Lahtinen <maijalah@cubbli18-gold.local>
2022-10-31 14:35:45 +0200

    path corrections for publishing - testing

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(shinydashboard)
 library(plotly)
 
 data <-
-  readRDS("~/Documents/Introduction to Data Science/miniprojekti/miniproject_data.rds")
+  readRDS("~/Documents/Introduction to Data Science/miniprojekti/temperatureinfinland/miniproject_data.rds")
 
 sidebar <- dashboardSidebar(
   menuItem("Utsjoki", tabName = "utsjoki", icon = icon("dashboard")),
