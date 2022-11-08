ab91a4d17f23233e3d973d27f7ba305c6b8d00f2
Maija Lahtinen <maijalah@cubbli18-gold.local>
2022-11-08 15:58:25 +0200

    Package added to init.R

--- init.R
+++ init.R
@@ -4,7 +4,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shinydashboard", "plotly", "ggplot2", "magrittr", "dplyr", "janitor")
+my_packages = c("shinydashboard", "plotly", "ggplot2", "magrittr", "dplyr", "janitor", "prophet")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
